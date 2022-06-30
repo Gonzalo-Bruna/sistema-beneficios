bbbded0bb3bdfd0ac5e28569fc340925405342a6
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2022-06-30 17:58:39 -0400

    transform beneficios to capital letters

--- src/app/interfaces/beneficio.ts
+++ src/app/interfaces/beneficio.ts
@@ -14,11 +14,11 @@ export interface Beneficio {
 }
 
 export const beneficios: Beneficio[] = [
-    {id_beneficio: 1, fk_tarjeta: 1, tarjeta: "Tarjeta Gonzalo", nombre: "Descuento Rappi", descripcion: "Descuento Comida", porcentaje_descuento: 30, ubicacion: "valparaiso", rubro: "comida", imagen: '../../assets/images/beneficios/rappi.png'},
-    {id_beneficio: 2, fk_tarjeta: 1, tarjeta: "Tarjeta Gonzalo", nombre: "Descuento Pedidosya", descripcion: "Descuento Comida", porcentaje_descuento: 25, ubicacion: "valparaiso", rubro: "comida", imagen: '../../assets/images/beneficios/pedidosya.png'},
-    {id_beneficio: 3, fk_tarjeta: 1, tarjeta: "Tarjeta Gonzalo", nombre: "Descuento Uber Eats", descripcion: "Descuento Comida", porcentaje_descuento: 25, ubicacion: "valparaiso", rubro: "comida", imagen: '../../assets/images/beneficios/ubereats.png'},
-    {id_beneficio: 4, fk_tarjeta: 3, tarjeta: "Tarjeta Cristian", nombre: "Descuento Falabella", descripcion: "Descuento Ropa", porcentaje_descuento: 10, ubicacion: "valparaiso", rubro: "ropa", imagen: '../../assets/images/beneficios/falabella.jpg'},
-    {id_beneficio: 5, fk_tarjeta: 3, tarjeta: "Tarjeta Cristian", nombre: "Descuento Ripley", descripcion: "Descuento Ropa", porcentaje_descuento: 10, ubicacion: "valparaiso", rubro: "ropa", imagen: '../../assets/images/beneficios/ripley.jpg'},
-    {id_beneficio: 6, fk_tarjeta: 3, tarjeta: "Tarjeta Cristian", nombre: "Descuento Adidas", descripcion: "Descuento Ropa", porcentaje_descuento: 10, ubicacion: "valparaiso", rubro: "ropa", imagen: '../../assets/images/beneficios/adidas.jpg'},
-    {id_beneficio: 7, fk_tarjeta: 5, tarjeta: "Tarjeta Diego", nombre: "Descuento Gimnasio Pacific", descripcion: "Descuento Gimnasio", porcentaje_descuento: 80, ubicacion: "valparaiso", rubro: "gimnasio", imagen: '../../assets/images/beneficios/pacific.jpg'}
+    {id_beneficio: 1, fk_tarjeta: 1, tarjeta: "Tarjeta Gonzalo", nombre: "Descuento Rappi", descripcion: "Descuento Comida", porcentaje_descuento: 30, ubicacion: "Valparaiso", rubro: "comida", imagen: '../../assets/images/beneficios/rappi.png'},
+    {id_beneficio: 2, fk_tarjeta: 1, tarjeta: "Tarjeta Gonzalo", nombre: "Descuento Pedidosya", descripcion: "Descuento Comida", porcentaje_descuento: 25, ubicacion: "Valparaiso", rubro: "comida", imagen: '../../assets/images/beneficios/pedidosya.png'},
+    {id_beneficio: 3, fk_tarjeta: 1, tarjeta: "Tarjeta Gonzalo", nombre: "Descuento Uber Eats", descripcion: "Descuento Comida", porcentaje_descuento: 25, ubicacion: "Valparaiso", rubro: "comida", imagen: '../../assets/images/beneficios/ubereats.png'},
+    {id_beneficio: 4, fk_tarjeta: 3, tarjeta: "Tarjeta Cristian", nombre: "Descuento Falabella", descripcion: "Descuento Ropa", porcentaje_descuento: 10, ubicacion: "Valparaiso", rubro: "ropa", imagen: '../../assets/images/beneficios/falabella.jpg'},
+    {id_beneficio: 5, fk_tarjeta: 3, tarjeta: "Tarjeta Cristian", nombre: "Descuento Ripley", descripcion: "Descuento Ropa", porcentaje_descuento: 10, ubicacion: "Valparaiso", rubro: "ropa", imagen: '../../assets/images/beneficios/ripley.jpg'},
+    {id_beneficio: 6, fk_tarjeta: 3, tarjeta: "Tarjeta Cristian", nombre: "Descuento Adidas", descripcion: "Descuento Ropa", porcentaje_descuento: 10, ubicacion: "Valparaiso", rubro: "ropa", imagen: '../../assets/images/beneficios/adidas.jpg'},
+    {id_beneficio: 7, fk_tarjeta: 5, tarjeta: "Tarjeta Diego", nombre: "Descuento Gimnasio Pacific", descripcion: "Descuento Gimnasio", porcentaje_descuento: 80, ubicacion: "Valparaiso", rubro: "gimnasio", imagen: '../../assets/images/beneficios/pacific.jpg'}
 ];
