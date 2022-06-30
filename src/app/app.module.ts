a99bf1049c6475e4b257be6ee844a1a4d24cdfbe
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2022-06-29 22:57:06 -0400

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CoverComponent } from './cover/cover.component';
+import { BeneficiosComponent } from './beneficios/beneficios.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CoverComponent,
+    BeneficiosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
