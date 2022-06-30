a8bd7e67056622cd48c4ab12809b3846a77d5536
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2022-06-30 00:02:46 -0400

    make use of beneficios service and interface

--- src/app/beneficios/beneficios.component.ts
+++ src/app/beneficios/beneficios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BeneficioService } from '../services/beneficio.service';
 
 @Component({
   selector: 'app-beneficios',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeneficiosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private beneficiosService: BeneficioService) { }
 
   ngOnInit(): void {
+  }
+
+  getBeneficios() {
+    return this.beneficiosService.getBeneficios();
   }
 
 }
