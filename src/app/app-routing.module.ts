338f0cd05dcd8baa92101929b89e86d0b2ad1538
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2022-06-30 17:59:41 -0400

    add new paths to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalleBeneficioComponent } from './detalle-beneficio/detalle-beneficio.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: HomeComponent
+  },
+  {
+    path: 'beneficio/:id', 
+    component: DetalleBeneficioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
